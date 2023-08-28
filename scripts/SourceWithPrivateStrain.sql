f8cb1c95e726f97bb55233b64be2479be41e9aaf
Dave Miers <dave.miers@jax.org>
2023-08-28 14:03:28 -0400

    Moved to pipeline report

--- scripts/SourceWithPrivateStrain.sql
+++
@@ -1,7 +0,0 @@
-\echo ''
-\echo 'Sources that have a private strain'
-\echo ''
-select * from prb_source so
-where exists (select 1 from prb_strain st
-              where st._strain_key = so._strain_key and
-                    st.private = 1);
