eec78f533c2a57381d22caf8942f19ef1d34f699
Dave Miers <dave.miers@jax.org>
2023-08-28 14:07:07 -0400

    Split off pipeline reports

--- runSanityChecks.sh
+++ runSanityChecks.sh
@@ -5,38 +5,43 @@
 #
 
 cd `dirname $0`
+DIR=`pwd`
+
 . ./Configuration
 
 #
 # Initialize the log and report files.
 #
-LOG_FILE=${LOGDIR}/dbsanity.log
-RPT_FILE=${RPTDIR}/dbsanity.rpt
-rm -f ${LOG_FILE} ${RPT_FILE}
-touch ${LOG_FILE} ${RPT_FILE}
+LOG_FILE=${LOGDIR}/sanitychecks.log
+SANITY_RPT=${RPTDIR}/database_sanity.rpt
+PIPELINE_RPT=${RPTDIR}/pipeline_sanity.rpt
+rm -f ${LOG_FILE} ${SANITY_RPT} ${PIPELINE_RPT}
+touch ${LOG_FILE} ${SANITY_RPT} ${PIPELINE_RPT}
 
 echo `date`: Start sanity checks >> ${LOG_FILE}
 
 #
-# Run each SQL script and append the output to the report.
+# Run each SQL script in the general sanity check directory and append the
+# output to the database sanity report.
 #
-cd scripts
+echo "Report generated from: ${PG_DBSERVER}.${PG_DBNAME}" >> ${SANITY_RPT}
+cd ${DIR}/scripts
 for i in *.sql
 do
     echo `date`: $i >> ${LOG_FILE}
-    ${PSQL_EXEC} -q -h ${PG_DBSERVER} -d ${PG_DBNAME} -U ${PG_DBUSER} -f $i >> ${RPT_FILE}
+    ${PSQL_EXEC} -q -h ${PG_DBSERVER} -d ${PG_DBNAME} -U ${PG_DBUSER} -f $i >> ${SANITY_RPT}
 done
 
 #
 # If any of the queries returned any rows, there is a potential data issue.
 # Set the subject of the email based on whether there is an problem.
 #
-COUNT=`cat ${RPT_FILE} | grep "^(.*row.*)" | grep -v "(0 rows)" | wc -l`
+COUNT=`cat ${SANITY_RPT} | grep "^(.*row.*)" | grep -v "(0 rows)" | wc -l`
 if [ ${COUNT} -eq 0 ]
 then
     SUBJECT="Database Sanity Report (Successful)"
 else
-    SUBJECT="Database Sanity Report (Failed)"
+    SUBJECT="Database Sanity Report (Warning)"
 fi
 
 #
@@ -44,7 +49,39 @@ fi
 #
 if [ "${EMAIL_LIST}" != "" ]
 then
-     cat ${RPT_FILE} | mailx -s "${SUBJECT}" ${EMAIL_LIST}
+     cat ${SANITY_RPT} | mailx -s "${SUBJECT}" ${EMAIL_LIST}
+fi
+
+#
+# Run each SQL script in the pipeline sanity check directory and append the
+# output to the pipeline sanity report.
+#
+echo "Report generated from: ${PG_DBSERVER}.${PG_DBNAME}" >> ${PIPELINE_RPT}
+cd ${DIR}/scripts_pipeline
+for i in *.sql
+do
+    echo `date`: $i >> ${LOG_FILE}
+    ${PSQL_EXEC} -q -h ${PG_DBSERVER} -d ${PG_DBNAME} -U ${PG_DBUSER} -f $i >> ${PIPELINE_RPT}
+done
+
+#
+# If any of the queries returned any rows, there is a potential data issue.
+# Set the subject of the email based on whether there is an problem.
+#
+COUNT=`cat ${PIPELINE_RPT} | grep "^(.*row.*)" | grep -v "(0 rows)" | wc -l`
+if [ ${COUNT} -eq 0 ]
+then
+    SUBJECT="Pipeline Sanity Report (Successful)"
+else
+    SUBJECT="Pipeline Sanity Report (Failed)"
+fi
+
+#
+# Send the report to everyone defined in the email list.
+#
+if [ "${EMAIL_LIST}" != "" ]
+then
+     cat ${PIPELINE_RPT} | mailx -s "${SUBJECT}" ${EMAIL_LIST}
 fi
 
 echo `date`: End sanity checks >> ${LOG_FILE}
